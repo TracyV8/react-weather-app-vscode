88fb88f4602a711cd1ecbe78643995f06e678a50
TracyV8 <123339740+TracyV8@users.noreply.github.com>
2023-06-19 10:56:07 +0800

    Added Github link in Appjs and removed footer as not required.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import "./App.css";
 import Weather from "./Weather";
-import Footer from "./Footer";
 
 function App() {
   return (
-    <container>
-      <div className="App">
-        <h1> Weather Application Using React in VS Code </h1>
-        <Weather />
-        <Footer />
-      </div>
-    </container>
+    <div className="App">
+      <h1> Weather Application Using React in VS Code </h1>
+      <Weather />
+      <h6>
+        This project is part of my SheCodes React Assessment and is {""}
+        <a
+          href="https://github.com/TracyV8/react-weather-app-vscode"
+          target="_blank"
+          rel="noreferrer"
+        >
+          {""}open sourced on GitHub
+        </a>
+      </h6>
+    </div>
   );
 }
 
